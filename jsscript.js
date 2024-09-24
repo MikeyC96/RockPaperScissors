b6349d467310f1a96ed255199717ebe8563809b6
MikeyC96 <MikeyCoiro96@gmail.com>
2024-09-24 13:49:24 -0400

    added function that computer chooses between rock paper and scissors

--- jsscript.js
+++ jsscript.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+console.log("Hello World!");
+function getComputerChoice(){
+/* Choose randomly between Rock Paper or Scissors*/
+ let answer = (Math.floor(Math.random() * 3 + 1));
+
+ /* randomly generate a number between 1 2 or 3*/
+ if(answer === 1){
+    
+    return "Rock";}
+else if(answer === 2){
+    return "Paper";
+}
+else{
+    return "Scissors";
+}
+ }
